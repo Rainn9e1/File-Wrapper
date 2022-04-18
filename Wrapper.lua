8e9a4739b308bad34e1674b14220f731ca35bc7c
Bug <77031032+0zBug@users.noreply.github.com>
2022-04-18 13:35:22 -0500

    fileSize fix
    fixed fileSize for characters with more than 255 bytes and folders

--- Wrapper.lua
+++ Wrapper.lua
@@ -38,18 +38,32 @@ end
 
 --Bug
 Wrapper["fileSize"] = function(o, t)
-    t = t or 0
+	t = t or 0
     
-    if type(o) == "string" then if isfile(o) then return #readfile(o) end o = listfiles(o) elseif o == nil then o = listfiles("") end
-    for i, v in next, o do if isfile(v) then t = t + #readfile(v) elseif isfolder(v) then t = t + filesize(listfiles(v), t) end end
-    return t
+	if type(o) == "string" then 
+		if isfile(o) then
+			return #({string.byte(readfile(o), 1, -1)}) 
+		end 
+	elseif o == nil then 
+		o = listfiles("") 
+	end
+	
+    	for i, v in next, o do 
+		if isfile(v) then 
+			t = t + #({string.byte(readfile(o), 1, -1)}) 
+		elseif isfolder(v) then 
+			t = t + Wrapper["fileSize"](listfiles(v), t) 
+		end 
+	end
+	
+	return t
 end
 
 Wrapper["bytes"] = function(n)
-    local s = tostring(math.floor(n))
-    local s = string.sub(s, 1, ((#s - 1) % 3 ) + 1) .. ({" bytes", "Kb", "Mb", "GB", "TB"})[math.floor((#s - 1) / 3) + 1]
-    if n == 1 then s = s:sub(1, #s-1) end
-    return s
+	local s = tostring(math.floor(n))
+	local s = string.sub(s, 1, ((#s - 1) % 3 ) + 1) .. ({" bytes", "Kb", "Mb", "GB", "TB"})[math.floor((#s - 1) / 3) + 1]
+	if n == 1 then s = s:sub(1, #s-1) end
+	return s
 end
 
 return Wrapper
